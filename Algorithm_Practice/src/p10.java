7e78ee791f22306edc10d2fe5c660cb7d624c037
mkwkw <mkkw2047@gmail.com>
2021-01-23 16:39:30 +0900

    1로 만들기

--- Algorithm_Practice/src/p10.java
+++ Algorithm_Practice/src/p10.java
@@ -8,38 +8,42 @@ public class p10 {
 	private static int n;
 	private static int dp[] = new int [1000000];
 	private static int count;
-	private static int func(int n) {
-		
-		while(n!=1) {
-			if(n%3==0) {
-				System.out.println("n: "+n);
-				n= n/3;
-				System.out.println("n/3: "+n);
-			}
-			else if(n%2==0) {
-				System.out.println("n: "+n);
-				n= n/2;
-				System.out.println("n/2: "+n);
-			}
-			else {
-				System.out.println("n: "+n);
-				n= n-1;
-				System.out.println("n-1: "+n);
-			}
-			count++;
-		}
-		return count;
-	}
+	
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		n = Integer.parseInt(br.readLine());
-		
 		count = 0;
 		
-		int answer = func(n);
-		System.out.println(answer);
+		//기저조건
+		dp[1] = 0;
+		dp[2] = 1;
+		dp[3] = 1;
+		
+		for(int i=4; i<=n; i++) {
+			//초기화
+			int count1 = 1000000;
+			int count2 = 1000000;
+			int count3 = 1000000;
+			
+			//-1 먼저
+			count1 = 1+ dp[i-1];
+			//나누기2 먼저
+			if(i%2==0)
+				count2 = 1+dp[i/2];
+			//나누기3 먼저
+			if(i%3==0)
+				count3 = 1+dp[i/3];
+			
+			//count1,2,3 중 가장 작은 값 찾기
+			int a = Math.min(count1, count2);
+			count = Math.min(count3, a);
+			
+			dp[i]= count;
+		}
+		br.close();
+		
+		System.out.println(dp[n]);
 		
 	}
 
