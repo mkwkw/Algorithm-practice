f978db92d105f01bac158ce0f3f409a69e6d6e3a
mkwkw <mkkw2047@gmail.com>
2021-01-26 18:38:15 +0900

    주석

--- Algorithm_Practice/src/p16.java
+++ Algorithm_Practice/src/p16.java
@@ -7,14 +7,16 @@ public class p16 {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		//입력
 		String arr[] = br.readLine().split(" ");
 		int n = Integer.parseInt(arr[0]);
 		int m = Integer.parseInt(arr[1]);
 		
 		String arr1[] = br.readLine().split(" ");
 		int size[] = new int [n];
-		int sum = 0;
-		int max = 0;
+		
+		int sum = 0; //합계
+		int max = 0; //가장 큰 값
 		for(int i=0; i<n; i++) {
 			size[i] = Integer.parseInt(arr1[i]);
 			if(max<size[i])
@@ -23,36 +25,34 @@ public class p16 {
 		}
 		
 		
-		int narr[] = new int [100001];
-		for(int i=0; i<100001; i++) {
-			narr[i]=0;
-		}
-		int left = max;
+		int left = max; 
 		int right = sum;
 		int mid = 0;
 		
 		while(left<=right) {
 			int bcount = 0; //블루레이 개수
-			int bsize = 0; //블루레이 크기
+			int bsize = 0; //하나의 블루레이 크기
 			
 			mid = (left+right)/2;
 			
 			for(int i=0; i<n; i++) {
-				if(bsize + size[i] > mid) {
+				if(bsize + size[i] > mid) { //크기가 임의의 블루레이 크기를 넘으면 블루레이 개수를 하나 증가시키고 블루레이 크기는 0으로 리셋
 					bsize = 0;
 					bcount++;
 				}
-				bsize += size[i];
+				bsize += size[i]; //크기가 임의의 블루레이 크기를 안 넘으면 블루레이 크기에 추가
 				
 			}
 			
-			if(bsize != 0)
+			if(bsize != 0) { 
+				
 				bcount++;
+			}
 			
-			if(bcount <= m )
-				right = mid-1;
+			if(bcount <= m ) //블루레이의 개수가 m이하인 경우
+				right = mid-1; //크기가 작아지게 한다.
 			else
-				left = mid+1;
+				left = mid+1; //크기가 커지게 한다.
 		}
 		
 		System.out.println(left);
