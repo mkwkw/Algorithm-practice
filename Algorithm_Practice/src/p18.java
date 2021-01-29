9a00ba001b77fc60443dc6435c2a31610c96b23f
mkwkw <mkkw2047@gmail.com>
2021-01-29 23:30:24 +0900

    구간 합

---
+++ Algorithm_Practice/src/p18.java
@@ -0,0 +1,39 @@
+//백준 11659
+//구간 합 구하기4
+//누적 합
+import java.util.*;
+public class p18 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int arr[] = new int [n+1];
+		arr[0]=0;
+ 		for(int i=1; i<=5; i++) {
+			arr[i]=sc.nextInt();
+		}
+ 		
+ 		//누적합
+ 		int prefixsum[] = new int [n+1];
+ 		prefixsum[0] = arr[0];
+ 		for(int i=1; i<=5; i++) {
+ 			prefixsum[i] = prefixsum[i-1]+arr[i];
+ 			
+ 		}
+ 		
+ 		int a[] = new int [m];
+ 		int b[] = new int [m];
+ 		
+ 		for(int i=0; i<m; i++) {
+ 			a[i] = sc.nextInt();
+ 			b[i] = sc.nextInt();
+ 		}
+ 		
+ 		for(int i=0; i<m; i++) {
+ 			System.out.println(prefixsum[b[i]]-prefixsum[a[i]-1]);
+ 		}
+ 		sc.close();
+	}
+}
