e2793fdbf30df72989d85f16d19fc5ee225642fa
Kyoungwon-Min <76611903+mkwkw@users.noreply.github.com>
2021-07-19 23:14:29 +0900

    Add files via upload

---
+++ Algorithm_Practice/c++/p4.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main(){
+	int n;
+	
+	cin>>n;	
+	vector<int> v(n);
+	
+	for(int i=0; i<n; i++){
+		cin>>v[i];
+	}
+	
+	//오름차순 정렬 
+	sort(v.begin(), v.end());
+	
+	int size = v.size();
+	int sum=0;
+	int result=0;
+	
+	//제일 큰 변 3개 골라서 삼각형 조건 만족하는지 판별 
+	for(int i=size-1; i>1; i--){
+		int c = v[i], b=v[i-1], a=v[i-2];
+		if(c<a+b) {
+			sum= a+b+c;
+			break;
+		}
+		
+	}
+	
+	if(sum==0) result=-1; //삼각형 조건 만족하는 것 없을 때 
+	else result = sum; //삼각형 조건 만족하는 것 있을 때 
+	
+	cout<<result;
+
+}
