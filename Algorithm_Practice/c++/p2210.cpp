95f38dd2dda89b79a5712c8246e9099b71f379a3
Kyoungwon-Min <76611903+mkwkw@users.noreply.github.com>
2021-08-17 22:57:03 +0900

    Update p2210.cpp

--- Algorithm_Practice/c++/p2210.cpp
+++ Algorithm_Practice/c++/p2210.cpp
@@ -2,10 +2,35 @@
 #include <vector>
 
 using namespace std;
+vector<int> v[5];
+bool visited[1000000];
+int result=0;
+
+int dx[] = { 0,0,1,-1 };
+int dy[] = { 1,-1,0,0 };
+
+void dfs(int x, int y, int sum, int cnt) {
+	if (cnt == 5) {
+		if (visited[sum] == false) {
+			visited[sum] = true;
+			result++;
+		}
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5) {
+			dfs(nx, ny, sum * 10 + v[nx][ny], cnt+1); //다른 숫자 얻기 위해 sum*10+v[nx][ny]으로 계산
+		}
+	}
+}
 
 int main() {
-	vector<int> v[5];
 	int a;
+
 
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
@@ -14,6 +39,13 @@ int main() {
 		
 		}
 	}
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			dfs(i, j, v[i][j], 0);
+		}
+	}
+	cout << result;
 
 
 }
