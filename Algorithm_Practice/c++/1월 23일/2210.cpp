fb8fcb99344aab3e110f99ac16d9e4df11d36473
Kyoungwon Min <mkkw2047@gmail.com>
2022-01-23 21:59:23 +0900

    [백준] 세 문제 정리 -ing

---
+++ Algorithm_Practice/c++/1월 23일/2210.cpp
@@ -0,0 +1,52 @@
+//중 - 숫자판 점프
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> v[5];
+bool visited[1000000];
+int result=0;
+
+int dx[] = { 0,0,1,-1 };
+int dy[] = { 1,-1,0,0 };
+
+void dfs(int x, int y, int sum, int cnt) {
+	if (cnt == 5) {
+		if (visited[sum] == false) {
+			visited[sum] = true;
+			result++;
+		}
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5) {
+			dfs(nx, ny, sum * 10 + v[nx][ny], cnt+1);
+		}
+	}
+}
+
+int main() {
+	int a;
+
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cin >> a;
+			v[i].push_back(a);
+		
+		}
+	}
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			dfs(i, j, v[i][j], 0);
+		}
+	}
+	cout << result;
+
+
+}
---
+++ Algorithm_Practice/c++/1월 23일/2607.cpp
@@ -0,0 +1,75 @@
+//하-비슷한 단어
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> alphabet; //비교 기준 알파벳 저장할 배열 (문자의 등장 횟수)
+
+int similar(int n, string str){
+    vector<int> newAlphabet;
+    int cnt=0;
+    int answer=0;
+
+    for(int t=0; t<26; t++){
+        //char a = 'A'+t;
+        newAlphabet.push_back(0);
+    }
+
+    for(int i=0; i<str.length(); i++){
+        //alphabet[str[i]-65].second++;
+        alphabet[str[i]-65]++;
+        //for(int j=0; j<26; j++)
+        //    cout<<alphabet[j].second<< ' ';
+    }
+
+
+    for(int i=0; i<n-1; i++){
+        cnt=0;
+        for(int t=0; t<26; t++){
+            //newAlphabet[t].second=0; //초기화
+            newAlphabet[t]=0;
+        }
+
+        string s;
+        cin>>s;
+        for(int j=0; j<s.length(); j++){
+            //newAlphabet[s[j]-65].second++;
+            newAlphabet[s[j]-65]++;
+        }
+
+        for(int p=0; p<26; p++){
+            //cnt += newAlphabet[p].second - alphabet[p].second;
+            cnt += abs(newAlphabet[p]-alphabet[p]);
+        }
+        //cout<<"cnt: "<<cnt<<'\n';
+        
+        if(cnt==0 || cnt==1 || (cnt==2&&s.length()==str.length())){
+            answer++;
+        }
+       
+    }
+    return answer;
+}
+
+int main(){
+    int n;
+    int answer;
+
+    cin>>n;
+
+    string str; //비교 기준
+    cin>>str;
+
+    alphabet.assign(26,0);
+
+    //for(int i=0; i<26; i++){
+        //char a = 'A'+i;
+        //alphabet.push_back(make_pair(a,0));
+    //}
+
+    answer = similar(n,str);
+    cout<<answer;
+   
+}
