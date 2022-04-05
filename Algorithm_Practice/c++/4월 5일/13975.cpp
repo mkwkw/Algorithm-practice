c1fbf91396872b123c6290b3ccac1d799ee60778
Kyoungwon Min <mkkw2047@gmail.com>
2022-04-05 22:50:11 +0900

    [백준] 우선순위 큐

--- Algorithm_Practice/c++/4월 5일/13975.cpp
+++ Algorithm_Practice/c++/4월 5일/13975.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    int n, a;
-    priority_queue<int> pq;
+long long integrateFiles(priority_queue<long long, vector<long long>, greater<long long>> &pq){
+    long long result = 0;
 
-    cin>>n;
-    
-    for(int i=0; i<n; i++){
-        cin>>a;
-
-        if(a==0){ //선물 가치 출력 - 선물 없으면 -1 출력
-            if(!pq.empty()){
-                cout<<pq.top()<<'\n';
-                pq.pop();
-            }
-            else{
-                cout<<"-1"<<'\n';
-            }
-        }
-        else{ // a개의 선물 충전
-            for(int j=0; j<a; j++){
-                int gift;
-                cin>>gift;
-                pq.push(gift);
-            }
-        }
+    while(pq.size()>=2){
+        long long add = 0;
+        add += pq.top();
+        pq.pop();
+        add += pq.top();
+        pq.pop();
+        result += add;
+        //cout<<result<<'\n';
+        pq.push(add);
     }
+
+    return result;
+}
+
+int main(){
+    //최대: k*파일크기: 10^10
+    priority_queue<long long, vector<long long>, greater<long long>> pq;
+    int t, k, file_size;
+    long long answer;
+
+    cin>>t;
+
+    for(int i=0; i<t; i++){
+        cin>>k;
+        for(int j=0; j<k; j++){
+            cin>>file_size;
+            pq.push(file_size);
+        }
+        answer = integrateFiles(pq);
+        cout<<answer<<'\n';
+        
+        pq.pop(); //integrateFiles에서 마지막 push 때문에 pq에 값이 하나 남는데 이거 빼줘야함.
+    }
+
 }
