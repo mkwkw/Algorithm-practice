af00567e7b4e08958bce19d588c4be78ff32ef8d
Kyoungwon Min <mkkw2047@gmail.com>
2023-03-19 23:35:37 +0900

    백준 [그리디] 수정 - 전역 변수 flag 설정하여 재귀함수 제어

--- Algorithm_Practice/c++/2023 상반기/3109.cpp
+++ Algorithm_Practice/c++/2023 상반기/3109.cpp
@@ -3,11 +3,13 @@
 
 using namespace std;
 
-int dr[3]={-1, 1, 0};
+int dr[3]={-1, 0, 1};
+//열 방향: 세가지 방향 모두 오른쪽으로 이동하므로 따로 배열 선언하지 않고 +1만 해줌
 
 vector<string> board;
 vector<vector<bool>> visited;
 int cnt = 0;
+bool flag; //마지막 열에 도달했는지 여부를 전역변수로 선언하여 전체적으로 통제한다. 마지막 열에 도달하면 뒤에 모든 재귀 건너뛰고 다음 행 첫번째 열로 오도록 함
 
 bool verify(int x, int y, int r, int c){
     if(x<0 || x>=r || y<0 || y>=c){
@@ -17,21 +19,12 @@ bool verify(int x, int y, int r, int c){
 }
 
 void countRoad(int x, int y, int r, int c){
-    
-    if(!verify(x,y,r,c) || visited[x][y]){
-        return;
-    }
-
-   
 
     visited[x][y] = true;
-    cout<<"move: "<<x<<' '<<y<<' '<<cnt<<'\n';
+    
     if(y==c-1){
         cnt++;
-        countRoad(x+1, 0, r, c);
-    }
-
-    if(x>=r-1){
+        flag = true; //마지막 열에 도달했다 표시
         return;
     }
     
@@ -39,19 +32,27 @@ void countRoad(int x, int y, int r, int c){
 
         int nextX = x+dr[i];
         int nextY = y+1;
-        cout<<nextX<<' '<<nextY<<' '<<cnt<<'\n';
 
-        countRoad(nextX, nextY, r, c);
+        if(verify(nextX, nextY, r, c) && !visited[nextX][nextY]){            
+            countRoad(nextX, nextY, r, c);
+            if(flag){
+                return;
+            }
+        }
         
     }
 }
 
 int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     
     int r, c;
     cin>>r>>c;
 
-    visited.assign(r, vector<bool>(r, false));
+    visited.assign(r, vector<bool>(c, false));
 
     for(int i=0; i<r; i++){
         string road;
@@ -69,6 +70,7 @@ int main(){
 
     for(int i=0; i<r; i++){
         if(board[i][0]!='x'){
+            flag = false; //마지막 열에 도달하면 true로 바뀌도록
             countRoad(i,0,r,c);
         }
     }
