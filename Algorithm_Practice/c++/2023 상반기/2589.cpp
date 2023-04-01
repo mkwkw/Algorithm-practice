5bbbc28cab9d9d93e5b2db742c221bb764966c4f
Kyoungwon Min <mkkw2047@gmail.com>
2023-04-01 17:21:03 +0900

    백준 [bfs] 성공

--- Algorithm_Practice/c++/2023 상반기/2589.cpp
+++ Algorithm_Practice/c++/2023 상반기/2589.cpp
@@ -1,119 +1,80 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-//dfs - 시간초과
-//bfs로 풀어보기!!
+//dfs로 풀면 시간 초과 발생
+//bfs로 풀면 성공 - 확실히 속도면에서는 bfs가 빠르다!
 
-vector<vector<char>> board;
-//vector<vector<bool>> visited;
-//vector<vector<bool>> visited1;
-//vector<vector<pair<int, int>>> point; // point.size(): 지역의 개수, point[i]: i번째 지역에 해당하는 육지의 좌표 배열
-//vector<pair<int, int>> oneDistrict;
-vector<vector<int>> dist;
-
-int dr[4] = {1,-1,0,0};
-int dc[4] = {0,0,1,-1};
-int answer = 0;
-
-
-bool verify(int r, int c, int n, int m){
-    if(r<0 || r>=n || c<0 || c>=m){
-        return false;
-    }
-    return true;
-}
-
-void findLand(int r, int c, int n, int m){
-    
-    if(/*visited[r][c] ||*/ board[r][c]=='W'){
-        return;
-    }
-
-    //visited[r][c] = true;
-    //oneDistrict.push_back({r,c});
-
-    for(int i=0; i<4; i++){
-        if(verify(r+dr[i], c+dc[i], n, m) && board[r+dr[i]][c+dc[i]]=='L' && dist[r][c]+1 < dist[r+dr[i]][c+dc[i]]/*!visited[r+dr[i]][c+dc[i]]*/){
-            dist[r+dr[i]][c+dc[i]] = min(dist[r][c]+1, dist[r+dr[i]][c+dc[i]]);
-            findLand(r+dr[i], c+dc[i], n, m);
-        }
-    }
-
-}
-
-/*
-void shortestPath(int r, int c, int n, int m){
-    
-    //visited1[r][c] = true;
-
-    for(int i=0; i<4; i++){
-        //cout<<"p: "<<r+dr[i]<<' '<<c+dc[i]<<' '<<verify(r+dr[i], c+dc[i], n, m)<<"\n";
-        if(verify(r+dr[i], c+dc[i], n, m) /*&& visited[r+dr[i]][c+dc[i]] && (!visited1[r+dr[i]][c+dc[i]] || (visited1[r+dr[i]][c+dc[i]] && (dist[r][c]+1 < dist[r+dr[i]][c+dc[i]])){
-            dist[r+dr[i]][c+dc[i]] = min(dist[r][c]+1, dist[r+dr[i]][c+dc[i]]);
-            
-            //answer = max(answer, dist[r+dr[i]][c+dc[i]]);
-            
-            //cout<<"move: "<<r+dr[i]<<' '<<c+dc[i]<<' '<<dist[r+dr[i]][c+dc[i]]<<'\n';
-            shortestPath(r+dr[i], c+dc[i], n, m);
-        }
-        else{
-            continue;
-        }
-    }
-    
-}
-*/
+struct Pos{
+    int r; //해당 좌표의 행
+    int c; //해당 좌표의 열
+    int cnt; //해당 좌표까지의 이동 시간
+};
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    int n, m, answer=0;
+    vector<string> board;
+    vector<vector<int>> time;
+    vector<vector<bool>> visited;
+    queue<Pos> q;
+    int dr[4]={1,-1,0,0};
+    int dc[4]={0,0,1,-1};
 
-
-    int n, m;
-    
     cin>>n>>m;
 
-    board.assign(n, vector<char>(m, ' '));
-    dist.assign(n, vector<int>(m, 3000));
+    board.assign(n,"");
 
-    string lw = "";
     for(int i=0; i<n; i++){
-        cin>>lw;
-        for(int j=0; j<m; j++){
-            board[i][j] = lw[j];
-        }
+        cin>>board[i];
     }
 
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
             if(board[i][j]=='L'){
-                dist.assign(n, vector<int>(m, 3000));
-                dist[i][j]=0;
+                time.assign(n, vector<int>(m, 3000));
+                visited.assign(n, vector<bool>(m, false));
+                
+                time[i][j]=0;
+                visited[i][j]=true;
+                q.push({i,j,0});
 
-                findLand(i, j, n, m);
-                
-                for(int k=0; k<n; k++){
-                    for(int h=0; h<m; h++){
-                        cout<<dist[k][h]<<' ';
-                        if(dist[k][h]>0 && dist[k][h]<3000){
-                            answer = max(answer, dist[k][h]);
+                while(!q.empty()){
+                    int r = q.front().r;
+                    int c = q.front().c;
+                    int cnt = q.front().cnt;
+
+                    for(int k=0; k<4; k++){
+                        int nextR = r+dr[k];
+                        int nextC = c+dc[k];
+
+                        if(nextR<0 || nextR>=n || nextC<0 || nextC>=m){
+                            continue;
                         }
+                        
+                        if(board[nextR][nextC]=='W'){
+                            continue;
+                        }
+
+                        //이미 방문을 한 좌표에 대해서는 시간 갱신만 진행
+                        if(visited[nextR][nextC]){
+                            time[nextR][nextC] = min(cnt+1, time[nextR][nextC]);
+                        }
+                        else{ //방문하지 않았던 좌표는 시간 갱신 및 큐에 넣고 방문 체크
+                            time[nextR][nextC] = cnt+1;
+                            q.push({nextR, nextC, time[nextR][nextC]});
+                            visited[nextR][nextC]=true;
+                        }
+
+                        answer = max(answer, time[nextR][nextC]);
                     }
-                    cout<<'\n';
+                    q.pop();
                 }
-                
+
             }
         }
     }
 
-    //각 지역의 가장 짧은 최단 거리 구하고, 지역끼리 가장 긴 최단 거리 비교
-    //
-
-    
     cout<<answer;
-    
-
 }
