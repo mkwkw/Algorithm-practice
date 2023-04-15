e36b46ec8197e90a78684b518ee5fd19df0e702c
Kyoungwon Min <mkkw2047@gmail.com>
2023-04-15 18:03:23 +0900

    Softeer [Level1, 구현]

---
+++ Algorithm_Practice/c++/2023 상반기/Softeer/A+B.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int T, A, B;
+	cin>>T;
+	for(int i=0; i<T; i++){
+		cin>>A>>B;
+		cout<<"Case #"<<i+1<<": "<<A+B<<'\n';
+	}
+
+	return 0;
+}
