ea287b9a764ca53d6d42aa373c59261f45be7170
Kyoungwon Min <mkkw2047@gmail.com>
2023-05-28 20:51:08 +0900

    백준 [구현]

--- Algorithm_Practice/c++/2023 상반기/14890.cpp
+++ Algorithm_Practice/c++/2023 상반기/14890.cpp
@@ -4,25 +4,101 @@
 
 using namespace std;
 
-struct Pos{
-    int r;
-    int c;
-    bool dir; //true: 행, false: 열
-};
+//범위 조심!!
+int countRoad(vector<vector<int>> &board, int n, int l){
+    vector<vector<bool>> visited;
+    int answer = 0;
+    visited.assign(n, vector<bool>(n, false));
+
+    for(int i=0; i<n; i++){
+        vector<int> pos;
+        bool flag = true; //유효성
+        for(int j=0; j<n-1; j++){
+            if(board[i][j]>board[i][j+1]){
+                if(board[i][j]-board[i][j+1]>1){
+                    flag = false;
+                    break;
+                }
+                pos.push_back(j+1);
+            }
+            else if(board[i][j]<board[i][j+1]){
+                if(board[i][j+1]-board[i][j]>1){
+                    flag = false;
+                    break;
+                }
+                pos.push_back(j);
+            }
+        }
+
+        if(!flag){
+            continue;
+        }
+
+        for(int j=0; j<pos.size(); j++){
+            int p = pos[j];
+            //cout<<"r: "<<i<<" c: "<<p<<'\n';
+            
+            
+            //else{
+                //왼쪽이 높은 경우
+                if(board[i][p-1]>board[i][p]){
+                    for(int k=p; k<p+l; k++){
+                        if(p+l>n || (p+l<=n && !(board[i][k]==board[i][p] && !visited[i][k]))){
+                            flag = false;
+                            break;
+                        }
+                    }
+
+                    if(flag){
+                        for(int k=p; k<p+l; k++){
+                            visited[i][k]=true;
+                        }
+                    }
+
+                }
+                //오른쪽이 높은 경우
+                else if(board[i][p]<board[i][p+1]){
+                    for(int k=p; k>p-l; k--){
+                        if(p-l<-1 || (p-l>=-1 && !(board[i][k]==board[i][p] && !visited[i][k]))){
+                            flag = false;
+                            break;
+                        }
+                    }
+
+                    if(flag){
+                        for(int k=p; k>p-l; k--){
+                            visited[i][k]=true;
+                        }
+                    }
+                }
+
+                if(!flag){
+                    break;
+                }
+            //}
+        }
+
+        if(!flag){
+            continue;
+        }
+        else{
+            //cout<<"r: "<<i<<'\n';
+            answer++;
+        }
+    }
+
+    return answer;
+}
 
 int main(){
-    vector<vector<int>> board;
-    vector<vector<bool>> check1; //행 체크
-    vector<vector<bool>> check2; //열 체크
-    queue<Pos> pos;
+    vector<vector<int>> board, board1;
+    int n, l;
     
-    int n, l, answer = 0;
-
     cin>>n>>l;
 
     board.assign(n, vector<int>(n, 0));
-    check1.assign(n, vector<bool>(n, true));
-    check2.assign(n, vector<bool>(n, true));
+    board1.assign(n, vector<int>(n, 0));
+    
 
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
@@ -31,255 +107,13 @@ int main(){
     }
 
     for(int i=0; i<n; i++){
-        int prev1 = 0, next1 = 0, prev2 = 0, next2 = 0;
-        
-        prev1 = board[i][0]; //행
-        prev2 = board[0][i]; //열
-        
-        for(int j=1; j<n; j++){
-            next1 = board[i][j];
-            next2 = board[j][i];
-
-            if(prev1!=next1){
-                //cout<<"row: "<<i<<' '<<j<<'\n';
-                check1[i][j]=false;
-                pos.push({i, j, true});
-            }
-
-            if(prev2!=next2){
-                //cout<<"col: "<<j<<' '<<i<<'\n';
-                check2[j][i]=false;
-                pos.push({j, i, false});
-            }
-
-            prev1 = board[i][j];
-            prev2 = board[j][i];
-            
-        }
-    }
-
-    while(!pos.empty()){
-        Pos p = pos.front();
-        int r = p.r;
-        int c = p.c;
-        bool dir = p.dir; //true: 행, false: 열
-
-        cout<<r<<' '<<c<<' '<<(dir?"row":"col")<<'\n';
-
-        if(dir){ //행 조사
-            int sameNum1 = 0, sameNum2 = 0;
-            //왼쪽
-            if(c>0){ 
-                if(board[r][c-1] < board[r][c]){//왼쪽 높이가 낮을 때
-                
-                    for(int i=c-1; i>=0; i--){
-                        if(board[r][i]==board[r][c-1]){
-                            sameNum1++;
-                        }
-                        else{
-                            break;
-                        }
-
-                        if(sameNum1==l){
-                            break;
-                        }
-                    }
-
-                }
-                else{ //왼쪽 높이가 높을 때
-
-                    for(int i=c; i<n; i++){
-                        if(board[r][i]==board[r][c]){
-                            sameNum1++;
-                        }
-                        else{
-                            break;
-                        }
-
-                        if(sameNum1==l){
-                            break;
-                        }
-                    }
-
-                }
-
-            }
-            //오른쪽
-            if(c<n-1){
-                if(board[r][c] > board[r][c+1]){ //오른쪽 높이가 낮을 때
-                
-                    for(int i=c+1; i<n; i++){
-                        if(board[r][i]==board[r][c+1]){
-                            sameNum2++;
-                        }
-                        else{
-                            break;
-                        }
-
-                        if(sameNum2==l){
-                            break;
-                        }
-                    }
-                }
-                else{ //오른쪽 높이가 높을 때
-                    
-                    for(int i=c; i>=0; i--){
-                        if(board[r][i]==board[r][c]){
-                            sameNum2++;
-                        }
-                        else{
-                            break;
-                        }
-
-                        if(sameNum2==l){
-                            break;
-                        }
-                    }
-
-                }
-            }
-            cout<<"row: "<<sameNum1<<' '<<sameNum2<<'\n';
-            if(sameNum1==l && sameNum2==l){
-                check1[r][c]=true;
-                
-            }
-            else if(sameNum1==0 && sameNum2==l){
-                check1[r][c]=true;
-            }
-            else if(sameNum1==l && sameNum2==0){
-                check1[r][c]=true;
-            }
-
-        }
-        else{ //열 조사
-            //위쪽
-            //아래쪽
-            int sameNum1 = 0, sameNum2 = 0;
-            //위쪽
-            if(r>0){
-
-                if(board[r-1][c] < board[r][c]){ //위쪽 높이가 낮을 때
-                
-                    for(int i=r-1; i>=0; i--){
-                        if(board[i][c]==board[r-1][c]){
-                            sameNum1++;
-                        }
-                        else{
-                            break;
-                        }
-
-                        if(sameNum1==l){
-                            break;
-                        }
-                    }
-                }
-                else{ //위쪽 높이가 높을 때
-                    
-                    for(int i=r; i<n; i++){
-                        if(board[i][c]==board[r][c]){
-                            sameNum1++;
-                        }
-                        else{
-                            break;
-                        }
-
-                        if(sameNum1==l){
-                            break;
-                        }
-                    }
-
-                }
-
-            }
-            //아래쪽
-            if(r<n-1){
-                if(board[r+1][c] < board[r][c]){ //아래쪽 높이가 낮을 때
-                
-                    for(int i=r+1; i<n; i++){
-                        if(board[i][c]==board[r+1][c]){
-                            sameNum2++;
-                        }
-                        else{
-                            break;
-                        }
-
-                        if(sameNum2==l){
-                            break;
-                        }
-                    }
-                }
-                else{ //아래쪽 높이가 높을 때
-                    for(int i=r; i>=0; i--){
-                        if(board[i][c]==board[r][c]){
-                            sameNum2++;
-                        }
-                        else{
-                            break;
-                        }
-
-                        if(sameNum2==l){
-                            break;
-                        }
-                    }
-
-                }
-            }
-
-            if(sameNum1==l && sameNum2==l){
-                check2[r][c]=true;
-            }
-            else if(sameNum1==0 && sameNum2==l){
-                check2[r][c]=true;
-            }
-            else if(sameNum1==l && sameNum2==0){
-                check2[r][c]=true;
-            }
-
-        }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                cout<<check1[i][j]<<' ';
-            }
-            cout<<'\n';
-        }
-
-        cout<<'\n';
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                cout<<check2[i][j]<<' ';
-            }
-            cout<<'\n';
-        }
-        
-        pos.pop();
-    }
-
-    for(int i=0; i<n; i++){
-        bool flag1 = true, flag2 = true;
-
         for(int j=0; j<n; j++){
-            if(!check1[i][j]){
-                flag1 = false;
-            }
-            if(!check2[j][i]){
-                flag2 = false;
-            }
-        }
-
-        if(flag1){
-            answer++;
-        }
-        if(flag2){
-            answer++;
+            board1[i][j] = board[j][i];
         }
     }
 
-    
-    //겹치는 거 해결: -_- 또는 _-_ 이런 경우, 들어간 부분 / 튀어나온 부분의 길이가 2*sameNum보다 작으면 겹친다.
-    
+    int horizontalCnt = countRoad(board, n, l);
+    int verticalCnt = countRoad(board1,n,l);
 
-    cout<<answer;
-
+    cout<<horizontalCnt+verticalCnt;
 }
