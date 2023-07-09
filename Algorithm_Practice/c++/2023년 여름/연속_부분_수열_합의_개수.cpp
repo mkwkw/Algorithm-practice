15e8823d10f04f3a64df4736dc911a551e765a62
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-09 17:54:43 +0900

    주석 변경 (원형 큐->원형 배열)

--- Algorithm_Practice/c++/2023년 여름/연속_부분_수열_합의_개수.cpp
+++ Algorithm_Practice/c++/2023년 여름/연속_부분_수열_합의_개수.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //중복 없이 -> set
-//원형 큐
+//원형 배열
 int solution(vector<int> elements) {
     set<int> partSum; //수의 범위<=int
     int answer = 0;
@@ -14,7 +14,7 @@ int solution(vector<int> elements) {
         for(int j=0; j<elements.size(); j++){ //시작 인덱스
             int sum = 0;
             for(int k=0; k<i; k++){
-                sum += elements[(j+k)%elements.size()]; //원형 큐 인덱스 특성 사용 -> [(j+k)%elements.size()]
+                sum += elements[(j+k)%elements.size()]; //원형 배열 인덱스 특성 사용 -> [(j+k)%elements.size()]
             }
             partSum.insert(sum);
         }
