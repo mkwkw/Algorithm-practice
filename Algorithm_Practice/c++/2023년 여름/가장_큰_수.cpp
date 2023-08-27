ae243a80efb64210d4756158747fb57786cbc597
Kyoungwon Min <mkkw2047@gmail.com>
2023-08-27 22:15:45 +0900

    [프로그래머스] Lv2 - 가장 큰 수 - 정렬, 예외처리, map

--- Algorithm_Practice/c++/2023년 여름/가장_큰_수.cpp
+++ Algorithm_Practice/c++/2023년 여름/가장_큰_수.cpp
@@ -1,54 +1,57 @@
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
 #include <iostream>
 
 using namespace std;
 
-//모든 경우의 수로 붙여보고 그다음 정렬
-vector<string> numberStr;
-vector<bool> visited;
-string tempStr;
-
-void dfs(string temp){
-    for(int i=0; i<visited.size(); i++){
-        if(!visited[i]){
-            temp += numberStr[i];
-            visited[i] = true;
-            dfs(temp);
-            visited[i] = false;
-        }
-    }
-    tempStr = temp;
+//정렬
+//같은 숫자로 시작하는 수만 정렬
+//0만 들어왔을 때 체크!! - 예외처리!
+bool comp(const string &a, const string &b){
+    //a와 b를 붙였을 때, b와 a를 붙였을 때 어느것이 큰가 비교
+    return stoi(a+b)>stoi(b+a);
+    
 }
 
 string solution(vector<int> numbers) {
-    string answer = "0";
-    char firstNum;
+    vector<string> numberStr;
+    string answer = "";
+    map<char, vector<string>> firstChAndNum;
     
     for(int i=0; i<numbers.size(); i++){
-        numberStr.push_back(to_string(numbers[i]));    
+        numberStr.push_back(to_string(numbers[i]));
+        firstChAndNum[numberStr[i][0]].push_back(numberStr[i]);
+    }
+        
+    //0만 들어왔을 때 판단 필요!
+    bool zeroflag = true;
+    
+    for(int i=9; i>=0; i--){
+        if(!firstChAndNum[i+'0'].empty()){
+            if(i+'0' != '0'){
+                zeroflag = false;
+            }
+            
+            if(firstChAndNum[i+'0'].size()==1){
+                answer += firstChAndNum[i+'0'][0];
+            }
+            else{ //같은 숫자로 시작하는 숫자 - 정렬 필요
+                string temp = "";
+                sort(firstChAndNum[i+'0'].begin(), firstChAndNum[i+'0'].end(), comp);
+                for(int j=0; j<firstChAndNum[i+'0'].size(); j++){
+                    //cout<<firstChAndNum[i+'0'][j]<<'\n';
+                    temp += firstChAndNum[i+'0'][j];
+                }
+                answer += temp;
+            }
+        }
     }
     
-    sort(numberStr.begin(), numberStr.end(), greater<>());
-    
-    firstNum = numberStr[0][0];
-    for(int i=0; i<numberStr.size(); i++){
-        if(firstNum == numberStr[i][0]){
-            visited.assign(numberStr.size(), false);
-            
-            visited[i] = true;
-            dfs(numberStr[i]);
-            cout<<tempStr<<'\n';
-            if(tempStr>answer){
-                answer = tempStr;
-            }
-        }            
+    if(zeroflag){ //0만 있다면 "0000" -> "0"
+        answer = "0";
     }
     
     return answer;
-}
-
-int main(){
-    cout<<"ans "<<solution({6,10,2});
 }
