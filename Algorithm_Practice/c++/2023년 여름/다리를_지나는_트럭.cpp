87d3bf797828c27f490cce54f4bf88e600692ccb
Kyoungwon Min <mkkw2047@gmail.com>
2023-08-27 21:42:22 +0900

    [프로그래머스] Lv2 - 다리를 지나는 트럭 - 큐, 구현

--- Algorithm_Practice/c++/2023년 여름/다리를_지나는_트럭.cpp
+++ Algorithm_Practice/c++/2023년 여름/다리를_지나는_트럭.cpp
@@ -5,50 +5,72 @@
 
 using namespace std;
 
+//큐
+//while문 잘 사용하기 - 조건에 따라 잘 나누기
+//조건에 따라 계속 넣고, 계속 빼고 
+//empty 체크 후 front 조회, idx 범위 체크 후 vector 접근
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     queue<int> bridge;
-    int answer = 0, currentLength = 0, currentWeight = 0, idx = 0;
+    int answer = 0, currentWeight = 0, idx = 0;
     
-    //처음 넣었을 때
-    bridge.push(truck_weights[0]);
-    answer = 1;
-    currentLength = 1;
-    currentWeight = truck_weights[0];
-    idx++;
-    
-    while(!bridge.empty()&&idx<truck_weights.size()){
-        while(currentWeight+truck_weights[idx]>weight){ //큐에 0 계속 넣어서 currentWeight+truck_weights[idx]가 weight보다 작거나 같을 때까지 앞에 거 나오도록 하기
-            //0 넣기
-            while(bridge.size()<bridge_length){
-                bridge.push(0);
-                answer++;
-            }
-            //하나빼고
-            if(bridge.front()>0){
-                currentLength--;
-            }
-            currentWeight -= bridge.front();
-            bridge.pop();
-        }
-        
+    //일단 조건 만족하면 계속 넣기
+    while(idx<truck_weights.size() && currentWeight+truck_weights[idx]<=weight && bridge.size()<bridge_length){
         currentWeight += truck_weights[idx];
-        bridge.push(truck_weights[idx]);
+        bridge.push(truck_weights[idx++]);
         answer++;
-        currentLength++;
     }
     
-    while(currentLength>0){
+    //다리를 바로 못들어가는 경우 - 0 넣어서 bridge 크기를 bridge_weight만큼 만든 다음에 빼야함.
+    while(bridge.size()<bridge_length){
         bridge.push(0);
-        if(bridge.front()>0){
-            currentLength--;
+        answer++;
+    }
+    
+    if(idx==truck_weights.size()){
+        while(currentWeight>0){
+            if(bridge.front()>0){
+                currentWeight -= bridge.front();
+            }
+            bridge.pop();
+            answer++;
         }
+        
+        return answer;
+    }
+    
+    //무게
+    while(idx<truck_weights.size()){
+        
+        //조건!!
+        while(!bridge.empty() && idx<truck_weights.size() && currentWeight-bridge.front()+truck_weights[idx]>weight){
+            currentWeight -= bridge.front();
+            bridge.pop();
+            if(currentWeight+truck_weights[idx]<=weight){
+                bridge.push(truck_weights[idx]);
+                currentWeight += truck_weights[idx++];
+            }
+            else{
+                bridge.push(0);
+            }
+
+            answer++;
+        }
+        
+        // currentWeight+truck_weights[idx]<=weight
+        while(!bridge.empty() && idx<truck_weights.size() && currentWeight-bridge.front()+truck_weights[idx]<=weight){
+            currentWeight -= bridge.front();
+            bridge.pop();
+            bridge.push(truck_weights[idx]);
+            currentWeight += truck_weights[idx++];
+            answer++;
+        }
+    }
+    
+    while(currentWeight>0){
+        currentWeight -= bridge.front();
         bridge.pop();
         answer++;
     }
     
     return answer;
-}
-
-int main(){
-    cout<<solution(2, 10, {7,4,5,6});
 }
