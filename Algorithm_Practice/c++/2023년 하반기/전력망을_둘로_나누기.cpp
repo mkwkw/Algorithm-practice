888b302593db56997f76c11188bc7d48e980be6d
Kyoungwon Min <mkkw2047@gmail.com>
2023-10-02 00:16:00 +0900

    [프로그래머스] Lv2 - 전력망을 둘로 나누기 - 완전탐색, 재귀

--- Algorithm_Practice/c++/2023년 하반기/전력망을_둘로_나누기.cpp
+++ Algorithm_Practice/c++/2023년 하반기/전력망을_둘로_나누기.cpp
@@ -4,20 +4,28 @@
 
 using namespace std;
 
+//완전탐색, 재귀
+//1. 인접 행렬 만들기 (n이 100 이하이므로 충분히 작음)
+//2. wires를 순차적으로 돌면서, [a,b]일 때, a와 b 사이에 전선을 끊는 것으로 가정 - 트리 구조이기 때문에 가능(사이클 X)
+//  a가 포함되는 네트워크의 송전탑 수, b가 포함되는 네트워크의 송전탑 수를 재귀로 계산 - visited 배열 이용
 vector<vector<bool>> connected;
+vector<bool> visited;
 int cnt;
 void findTowerCnt(int n, int startNode, int parentNode, int cutNode){
     cout<<n<<' '<<startNode<<' '<<parentNode<<' '<<cutNode<<" cnt "<<cnt<<'\n';
     bool flag = true;
     for(int i=0; i<n; i++){
-        if(connected[startNode][i] && i!=startNode && i!=cutNode && i!=parentNode){
+        if(connected[parentNode][i] && i!=startNode && i!=cutNode && i!=parentNode && !visited[i]){
             flag = false;
             ++cnt;
+            visited[i] = true;
+            cout<<"pass "<<n<<" parent "<<parentNode<<' '<<i<<' '<<'\n';
             findTowerCnt(n, startNode, i, cutNode);
         }
     }
     
     if(flag){
+        cout<<"end\n";
         return;
     }
     
@@ -47,10 +55,15 @@ int solution(int n, vector<vector<int>> wires) {
         int a = wires[i][0]-1;
         int b = wires[i][1]-1;
         
+        visited.assign(n, false);
+        visited[a] = true;
         cnt = 1;
         findTowerCnt(n, a, a, b); //네트워크 a
         int aCnt = cnt;
         cout<<"aCnt "<<aCnt<<'\n';
+
+        visited.assign(n, false);
+        visited[b] = true;
         cnt = 1;
         findTowerCnt(n, b, b, a); //네트워크 b
         int bCnt = cnt;
