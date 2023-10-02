a27065f23683b41ea4524e6e7b5f8f536c242bae
Kyoungwon Min <mkkw2047@gmail.com>
2023-10-02 21:48:26 +0900

    [프로그래머스] Lv2 - 괄호 변환 - 재귀, 스택

--- Algorithm_Practice/c++/2023년 하반기/괄호_변환.cpp
+++ Algorithm_Practice/c++/2023년 하반기/괄호_변환.cpp
@@ -6,8 +6,12 @@
 
 using namespace std;
 
+//재귀, 스택
+//1. 스택으로 올바른 문자열인지 확인
+//2-1. u가 올바른 문자열이라면 u+makeRightSyntax(v)
+//2-2. u가 올바르지 않은 문자열이라면 (makeRightSyntax(v))+앞뒤 떼고 괄호 반대로 바꾼 u
 string result = "";
-string v = "";
+string vPrime = "";
 bool verifySyntax(string p){
     stack<char> st;
     
@@ -26,15 +30,16 @@ bool verifySyntax(string p){
     return true;
 }
 
-void makeRightSyntax(string p){
+//string으로 리턴하도록!
+string makeRightSyntax(string p){
     //cout<<p<<'\n';
     int leftCnt = 0; // ( 개수
     int rightCnt = 0; // ) 개수
     int idx = 0;
-    string u;
+    string u,v;
     
     if(p=="" || verifySyntax(p)){
-        return;
+        return p;
     }
     
     for(int i=0; i<p.length(); i++){
@@ -54,21 +59,33 @@ void makeRightSyntax(string p){
     u = p.substr(0,idx+1);
     v = p.substr(idx+1);
     //cout<<"u "<<u<<" v "<<v<<'\n';
-    makeRightSyntax(v);
+    vPrime = makeRightSyntax(v); //재귀를 거친 v 만들기
     
+    //cout<<"v "<<vPrime<<'\n';
+    //u가 올바른 괄호 문자열일 때
     if(verifySyntax(u)){
-        //cout<<"final "<<u<<' '<<v<<'\n';
-        result = u+result;
+        //cout<<"final "<<u<<' '<<result<<'\n';
+        result = u+vPrime;
     }
-    else{
+    else{//u가 올바른 괄호 문자열이 아닐 때
         //cout<<"f ";
-        result = "(" + v + ")";
+        //cout<<"ffinal "<<u<<' '<<vPrime<<' '<<result<<'\n';
+        result = "(" + vPrime + ")";
         //cout<<result<<' ';
         u = u.substr(1,u.length()-2);
-        reverse(u.begin(), u.end());
+        for(int i=0; i<u.length(); i++){
+            if(u[i]==')'){
+                u[i] = '(';
+            }
+            else{
+                u[i] = ')';
+            }
+        }
         result += u;
         //cout<<"final "<<result<<'\n';
     }
+    
+    return result;
 }
 
 string solution(string p) {
