78114ff552952d50848f76701763e11be292d904
Kyoungwon Min <mkkw2047@gmail.com>
2023-10-05 20:04:20 +0900

    [프로그래머스] Lv2 - 가장 큰 정사각형 찾기 - dp

--- Algorithm_Practice/c++/2023년 하반기/가장_큰_정사각형_찾기.cpp
+++ Algorithm_Practice/c++/2023년 하반기/가장_큰_정사각형_찾기.cpp
@@ -1,42 +1,43 @@
 #include <iostream>
-#include<vector>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
-//완전 탐색 - 정확성 o, 효율성 시간초과
+//dp
+//[i][j]에서 나올 수 있는 최대 정사각형의 한변의 길이 구하기
+//board[i][j]==1일 때, dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
 int solution(vector<vector<int>> board)
 {
-    int answer = 0;
-    int length = min(board.size(), board[0].size()); //가능한 최대 정사각형 변의 길이 - min(행의 길이, 열의 길이)
+    vector<vector<int>> dp;
+    dp.assign(board.size(), vector<int>(board[0].size(), 0));
     
-    for(int l = length; l>=1; l--){
-        //cout<<"l "<<l<<'\n';
-        for(int i=0; i<=board.size()-l; i++){
-            for(int j=0; j<=board[0].size()-l; j++){
-                bool flag = true;
-                //cout<<i<<' '<<j<<'\n';
-                for(int k=i; k<i+l; k++){
-                    for(int t=j; t<j+l; t++){
-                        
-                        if(board[k][t]==0){
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if(!flag){
-                        break;
-                    }
-                }
-                //cout<<'\n';
-                if(flag){
-                    //cout<<"re\n";
-                    return  l*l;
-                }  
-            }
-            
-        }
-        
+    int answer = 0;
+    for(int i=0; i<board[0].size(); i++){
+        dp[0][i] = board[0][i];
     }
+    for(int i=0; i<board.size(); i++){
+        dp[i][0] = board[i][0];
+    }
+    
+    for(int i=1; i<board.size(); i++){
+        for(int j=1; j<board[0].size(); j++){
+            if(board[i][j]==1){ //board[i][j]가 1이어야 주변의 사각형에 이어서 사각형을 넓힐 수 있음.
+                dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])+1;
+            }
+        }
+    }
+    
+    //[1,0]
+    //[0,0]
+    //일 때의 답은 1이 되어야하므로 2차원 벡터 전체 탐색
+    for(int i=0; i<board.size(); i++){
+        for(int j=0; j<board[i].size(); j++){
+            answer = max(answer, dp[i][j]);
+        }
+    }
+    
+    answer *= answer;
     
     return answer;
 }
