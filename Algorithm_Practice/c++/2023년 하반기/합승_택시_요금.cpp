936af31db1ede5181f3e793e6b06da33d9c76ae6
Kyoungwon Min <mkkw2047@gmail.com>
2023-10-17 20:45:26 +0900

    [프로그래머스] Lv3 - 합승 택시 요금 - bfs, 다익스트라

--- Algorithm_Practice/c++/2023년 하반기/합승_택시_요금.cpp
+++ Algorithm_Practice/c++/2023년 하반기/합승_택시_요금.cpp
@@ -1,91 +1,39 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <queue>
 #include <iostream>
 #define INF 100000000
 
 using namespace std;
 
 //다익스트라
-//dfs -> 시간 초과
+//1. dfs -> 시간 초과
+//2. bfs - 큐 -> 효율성 테스트 케이스 7,8만 시간초과
+
 //최단 거리 구하기
 //양방향
 //s->모든 노드 최단 거리
 //a->모든 노드 최단 거리
 //b->모든 노드 최단 거리
 
-map<int, int> sToAll, aToAll, bToAll;
-map<int, vector<pair<int,int>>> fareMap;
-void findMinFareFromS(int n, int start){
-    //cout<<before<<' '<<start<<'\n';
-    for(int i=0; i<fareMap[start].size(); i++){
-        if(sToAll[fareMap[start][i].first]>=sToAll[start]+fareMap[start][i].second){
-            //cout<<"["<<fareMap[start][i].first<<"] = "<<sToAll[start]<<" + "<<fareMap[start][i].second<<'\n';
-            sToAll[fareMap[start][i].first] = sToAll[start]+fareMap[start][i].second;
-            findMinFareFromS(n, fareMap[start][i].first);
-        }
-        
-    }
-    // for(auto m : sToAll){
-    //     cout<<m.first<<' '<<m.second<<'\n';
-    // }
-    // cout<<'\n';
-       
-}
 
-void findMinFareFromA(int n, int start){
-    //cout<<before<<' '<<start<<'\n';
-    for(int i=0; i<fareMap[start].size(); i++){
-        if(aToAll[fareMap[start][i].first]>=aToAll[start]+fareMap[start][i].second){
-            //cout<<"["<<fareMap[start][i].first<<"] = "<<sToAll[start]<<" + "<<fareMap[start][i].second<<'\n';
-            aToAll[fareMap[start][i].first] = aToAll[start]+fareMap[start][i].second;
-            findMinFareFromA(n, fareMap[start][i].first);
-        }
-        
-    }
-    // for(auto m : sToAll){
-    //     cout<<m.first<<' '<<m.second<<'\n';
-    // }
-    // cout<<'\n';
-       
-}
-
-void findMinFareFromB(int n, int start){
-    //cout<<before<<' '<<start<<'\n';
-    for(int i=0; i<fareMap[start].size(); i++){
-        if(bToAll[fareMap[start][i].first]>=bToAll[start]+fareMap[start][i].second){
-            //cout<<"["<<fareMap[start][i].first<<"] = "<<sToAll[start]<<" + "<<fareMap[start][i].second<<'\n';
-            bToAll[fareMap[start][i].first] = bToAll[start]+fareMap[start][i].second;
-            findMinFareFromB(n, fareMap[start][i].first);
-        }
-        
-    }
-    // for(auto m : sToAll){
-    //     cout<<m.first<<' '<<m.second<<'\n';
-    // }
-    // cout<<'\n';
-       
-}
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
+    map<int, int> sToAll, aToAll, bToAll;
+    map<int, vector<pair<int,int>>> fareMap;
+
     int answer = 1000000001;
-    
+    queue<int> sQ, aQ, bQ; //노드 번호
+
     //초기화
-    //s->모든 노드 최단 거리
+    //s,a,b->모든 노드 최단 거리
     for(int i=1; i<=n; i++){
         sToAll[i] = INF;    
-    }
-    sToAll[s] = 0;
-    
-    //a->모든 노드 최단 거리
-    for(int i=1; i<=n; i++){
         aToAll[i] = INF;    
-    }
-    aToAll[a] = 0;
-    
-    //b->모든 노드 최단 거리
-    for(int i=1; i<=n; i++){
         bToAll[i] = INF;    
     }
+    sToAll[s] = 0;
+    aToAll[a] = 0;
     bToAll[b] = 0;
     
     //fareMap
@@ -93,22 +41,57 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
         fareMap[fares[i][0]].push_back({fares[i][1], fares[i][2]});
         fareMap[fares[i][1]].push_back({fares[i][0], fares[i][2]});    
     }
-    findMinFareFromS(n, s);
-    findMinFareFromA(n, a);
-    findMinFareFromB(n, b);
     
-    for(int i=1; i<=n; i++){
+    aQ.push(a);
+    while(!aQ.empty()){
+        int num = aQ.front();
+        //cout<<num<<'\n';
+        for(int i=0; i<fareMap[num].size(); i++){
+            if(aToAll[fareMap[num][i].first]>aToAll[num]+fareMap[num][i].second){
+                aToAll[fareMap[num][i].first] = aToAll[num]+fareMap[num][i].second;
+                aQ.push(fareMap[num][i].first);    
+            }
+        }
         
-        answer = min(answer, sToAll[i]+bToAll[i]+aToAll[i]);
+        aQ.pop();
     }
     
-    // for(auto m : fareMap){
-    //     for(int i=0; i<m.second.size(); i++)
-    //         cout<<m.first<<' '<<m.second[i].first<<' '<<m.second[i].second<<'\n';
-    // }
-    
-    
-    
+    bQ.push(b);
+    while(!bQ.empty()){
+        int num = bQ.front();
+        //cout<<num<<'\n';
+        for(int i=0; i<fareMap[num].size(); i++){
+            if(bToAll[fareMap[num][i].first]>bToAll[num]+fareMap[num][i].second){
+                bToAll[fareMap[num][i].first] = bToAll[num]+fareMap[num][i].second;
+                bQ.push(fareMap[num][i].first);    
+            }
+        }
         
+        bQ.pop();
+    }
+    
+    sQ.push(s);
+    while(!sQ.empty()){
+        int num = sQ.front();
+        //cout<<num<<'\n';    
+        for(int i=0; i<fareMap[num].size(); i++){
+            if(sToAll[fareMap[num][i].first]>sToAll[num]+fareMap[num][i].second){
+                sToAll[fareMap[num][i].first] = sToAll[num]+fareMap[num][i].second;
+                
+                sQ.push(fareMap[num][i].first);    
+            }
+            answer = min(answer, sToAll[fareMap[num][i].first]+bToAll[fareMap[num][i].first]+aToAll[fareMap[num][i].first]);
+        }
+        
+        sQ.pop();
+    }
+  
+    // for(int i=1; i<=n; i++){    
+    //     answer = min(answer, sToAll[i]+bToAll[i]+aToAll[i]);
+    // }
+     
+    // for(auto m : sToAll){
+    //     cout<<m.first<<' '<<m.second<<'\n';
+    // }
     return answer;
 }
